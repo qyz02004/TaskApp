32e7a1ec2394476cbbbb9ba0102edf2c8612b949
kouchi.fukushi <qyz02004@gmail.com>
2018-01-19 22:49:08 +0900

    4.4 ListView までを変更

--- app/src/main/java/jp/techacademy/kouchi/fukushi/taskapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/kouchi/fukushi/taskapp/MainActivity.java
@@ -5,8 +5,15 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.List;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView mListView;
+    private TaskAdapter mTaskAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +28,43 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // ListViewの設定
+        mTaskAdapter = new TaskAdapter(MainActivity.this);
+        mListView = (ListView) findViewById(R.id.listView1);
+
+        // ListViewをタップしたときの処理
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // 入力・編集する画面に遷移させる
+            }
+        });
+
+        // ListViewを長押ししたときの処理
+        mListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+                // タスクを削除する
+
+                return true;
+            }
+        });
+
+        reloadListView();
+    }
+
+    private void reloadListView() {
+
+        // 後でTaskクラスに変更する
+        List<String> taskList = new ArrayList<String>();
+        taskList.add("aaa");
+        taskList.add("bbb");
+        taskList.add("ccc");
+
+        mTaskAdapter.setTaskList(taskList);
+        mListView.setAdapter(mTaskAdapter);
+        mTaskAdapter.notifyDataSetChanged();
     }
 }
